5a6e32dbb42447e1317f9d134584a12ee5536109
Freddy Abrahamsson <freddy@abrahamsson.xyz>
2022-12-03 04:20:59 +0100

    Add docs to template.

--- templates/day_xx.rb
+++ templates/day_xx.rb
@@ -8,11 +8,19 @@ module Days
   # Day XX
   class DayXX < Day
     sig { returns(T.any(String, Integer)) }
+    ##
+    # Solution to part a of day XX.
+    #
+    # @return the answer to part a, day XX.
     def part_a
       super
     end
 
     sig { returns(T.any(String, Integer)) }
+    ##
+    # Solution to part b of day XX.
+    #
+    # @return the answer to part B, day XX.
     def part_b
       super
     end
