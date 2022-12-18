d6ec3b72a98dd014ad4ad9d00cd065bd33d40638
Freddy Abrahamsson <freddy@abrahamsson.xyz>
2022-12-18 03:19:09 +0100

    Update test reporter

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,6 +7,10 @@ SimpleCov.start
 $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 
 require "minitest/autorun"
+require "minitest/reporters"
 require "sorbet-runtime"
 
 require_relative "../lib/utils/filename_utils"
+
+reporter_options = { color: true, slow_count: 5 }
+Minitest::Reporters.use! [Minitest::Reporters::DefaultReporter.new(reporter_options)]
